e8ab4fe7b67996545392fed087f9f181954c43f3
Elia Darwish <elia.darwish@shetani.de>
2022-04-24 14:03:33 +0200

    Chenge homepage styles file name

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 import { useState } from 'react'
 import Head from 'next/head'
 
-import styles from 'styles/home.module.css'
+import styles from 'styles/Home.module.css'
 import { Checkbox } from 'components/Checkbox'
 import { Input } from 'components/Input'
 import { AtIcon } from 'components/icons'
